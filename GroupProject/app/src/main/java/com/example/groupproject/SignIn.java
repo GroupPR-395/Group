e2534df357d01eb982cb9ef21f3fd684acdd2cce
Minory54 <72006176+Minory54@users.noreply.github.com>
2020-12-12 20:49:11 +0700

    Add group project

--- GroupProject/app/src/main/java/com/example/groupproject/SignIn.java
+++ GroupProject/app/src/main/java/com/example/groupproject/SignIn.java
@@ -2,13 +2,29 @@ package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
+
+    Button test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        test = findViewById(R.id.button_sign_in);
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignIn.this, StartScreen.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
