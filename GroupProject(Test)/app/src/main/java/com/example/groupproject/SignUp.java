ee13d594b6e18a9809c7a5528548a1811448ddb7
TheFollenBest <67145215+TheFollenBest@users.noreply.github.com>
2020-12-17 21:46:47 +0700

    Upload "Test" file

---
+++ GroupProject(Test)/app/src/main/java/com/example/groupproject/SignUp.java
@@ -0,0 +1,18 @@
+package com.example.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUp extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
