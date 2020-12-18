3830b5310a3e7b618fd875eb8659247b1b32e80e
Minory54 <72006176+Minory54@users.noreply.github.com>
2020-12-18 12:10:22 +0700

    Add pre final build

--- GroupProject/app/src/main/java/com/example/groupproject/SignUp.java
+++ GroupProject/app/src/main/java/com/example/groupproject/SignUp.java
@@ -2,13 +2,29 @@ package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
+
+    Button log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        log = findViewById(R.id.button_sign_in);
+
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, SignIn.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
